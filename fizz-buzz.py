7929061b397ff30b94e1939add585de980e4260b
tiraboschi <tiraboschi@zoho.com>
2017-09-07 14:44:06 +0930

    switched Fizz and Buzz

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,10 +1,6 @@
 import os
 count = 0
 ct = input("What should I count up to? ")
-if os.name == "posix" or "mac": # If you're running linux/macos
-    os.system("clear") # bash etc. uses 'clear' whereas windows uses 'cls'
-elif os.name == "nt": # If you're running windows NT (2000 up)
-    os.system("cls") # cmd uses 'cls' instead of 'clear'
 while True: # Infinite loop
     if int(ct) < int(count): # If the number counted up to is higher than the max
         break # Cancel if above is true
@@ -12,9 +8,9 @@ while True: # Infinite loop
         if count % 5 == 0 and count % 3 == 0: # % calculates remainders
             print("Fizz-Buzz")                # So if count divided by 5 has
         elif count % 5 == 0:                  # has 0 left over it is divisible
-            print("Fizz")                     # by 5. Same with 3
+            print("Buzz")                     # by 5. Same with 3
         elif count % 3 == 0:                  # applying that logic makes reading
-            print("Buzz")                     # this code pretty easy
+            print("Fizz")                     # this code pretty easy
         else:
             print(count)                      # if you do get a remainder,
         count = count + 1                     # print normally :p
